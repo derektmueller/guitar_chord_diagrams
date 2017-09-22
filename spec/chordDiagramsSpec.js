6eac71d045a6007209d3fcbe8b472d0c05a497c4
Derek Mueller <dtmuelle@gmail.com>
2017-09-21 22:22:31 -0700

    add configurable fret count

--- spec/chordDiagramsSpec.js
+++ spec/chordDiagramsSpec.js
@@ -1,6 +1,6 @@
 describe("ChordDiagrams", () => {
   let ChordDiagrams = require('../lib/ChordDiagrams.js');
-  let chordDiagrams = new ChordDiagrams;
+  let chordDiagrams = new ChordDiagrams();
 
   // ideas: alternate tunings, variable fret count, number of strings
 
@@ -45,6 +45,30 @@ describe("ChordDiagrams", () => {
           [0, 0, 1, 0,  0, 0, 0, 1,  0, 0, 0, 1,  0, 0, 1, 0,  0, 0, 0, 1,  0, 0, 0, 1],
         // e  f  f# g   g# a  a# b   c  c# d  d#  e  f  f# g   g# a  a# b   c  c# d  d#
           [1, 0, 0, 0,  1, 0, 0, 1,  0, 0, 0, 0,  1, 0, 0, 0,  1, 0, 0, 1,  0, 0, 0, 0]
+        ]);    
+      });
+    });
+
+    describe("when specifying a diagram with fewer frets", () => {
+      let chordDiagrams = new ChordDiagrams({fretCount: 12});
+
+      it(`returns a multidimensional array of numbers with 1 for entries corresponding to notes 
+        passed in parameter`, () => {
+
+        expect(chordDiagrams.getDiagram('e g# b')).toEqual([
+        // e  f  f# g   g# a  a# b   c  c# d  d#  e
+          [1, 0, 0, 0,  1, 0, 0, 1,  0, 0, 0, 0,  1],
+        // b  c  c# d   d# e  f  f#  g  g# a  a#  b
+          [1, 0, 0, 0,  0, 1, 0, 0,  0, 1, 0, 0,  1],
+        // g  g# a  a#  b  c  c# d   d# e  f  f#  g
+          [0, 1, 0, 0,  1, 0, 0, 0,  0, 1, 0, 0,  0],
+
+        // d  d# e  f   f# g  g# a   a# b  c  c#  d
+          [0, 0, 1, 0,  0, 0, 1, 0,  0, 1, 0, 0,  0],
+        // a  a# b  c   c# d  d# e   f  f# g  g#  a
+          [0, 0, 1, 0,  0, 0, 0, 1,  0, 0, 0, 1,  0],
+        // e  f  f# g   g# a  a# b   c  c# d  d#  e
+          [1, 0, 0, 0,  1, 0, 0, 1,  0, 0, 0, 0,  1]
         ]);    
       });
     });
