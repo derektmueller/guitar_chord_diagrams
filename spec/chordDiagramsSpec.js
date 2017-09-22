5c383141c1294122740264eab273dfb54eebf3e5
Derek Mueller <dtmuelle@gmail.com>
2017-09-21 22:02:29 -0700

    add chord diagram for set of notes

---
+++ spec/chordDiagramsSpec.js
@@ -0,0 +1,52 @@
+describe("ChordDiagrams", () => {
+  let ChordDiagrams = require('../lib/ChordDiagrams.js');
+  let chordDiagrams = new ChordDiagrams;
+
+  // ideas: alternate tunings, variable fret count, number of strings
+
+  describe("getDiagram", () => {
+    describe("when passed a single note is passed in", () => {
+      it(`returns a multidimensional array of numbers with 1 for entries corresponding to notes 
+        passed in parameter`, () => {
+
+        expect(chordDiagrams.getDiagram('e')).toEqual([
+        // e  f  f# g   g# a  a# b   c  c# d  d#  e  f  f# g   g# a  a# b   c  c# d  d#
+          [1, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  1, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0],
+        // b  c  c# d   d# e  f  f#  g  g# a  a#  b  c  c# d   d# e  f  f#  g  g# a  a#
+          [0, 0, 0, 0,  0, 1, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 1, 0, 0,  0, 0, 0, 0],
+        // g  g# a  a#  b  c  c# d   d# e  f  f#  g  g# a  a#  b  c  c# d   d# e  f  f#
+          [0, 0, 0, 0,  0, 0, 0, 0,  0, 1, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 1, 0, 0],
+
+        // d  d# e  f   f# g  g# a   a# b  c  c#  d  d# e  f   f# g  g# a   a# b  c  c#
+          [0, 0, 1, 0,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 1, 0,  0, 0, 0, 0,  0, 0, 0, 0],
+        // a  a# b  c   c# d  d# e   f  f# g  g#  a  a# b  c   c# d  d# e   f  f# g  g#
+          [0, 0, 0, 0,  0, 0, 0, 1,  0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 1,  0, 0, 0, 0],
+        // e  f  f# g   g# a  a# b   c  c# d  d#  e  f  f# g   g# a  a# b   c  c# d  d#
+          [1, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0,  1, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 0]
+        ]);    
+      });
+    });
+
+    describe("when passed a space-delimited string of notes is passed in", () => {
+      it(`returns a multidimensional array of numbers with 1 for entries corresponding to notes 
+        passed in parameter`, () => {
+
+        expect(chordDiagrams.getDiagram('e g# b')).toEqual([
+        // e  f  f# g   g# a  a# b   c  c# d  d#  e  f  f# g   g# a  a# b   c  c# d  d#
+          [1, 0, 0, 0,  1, 0, 0, 1,  0, 0, 0, 0,  1, 0, 0, 0,  1, 0, 0, 1,  0, 0, 0, 0],
+        // b  c  c# d   d# e  f  f#  g  g# a  a#  b  c  c# d   d# e  f  f#  g  g# a  a#
+          [1, 0, 0, 0,  0, 1, 0, 0,  0, 1, 0, 0,  1, 0, 0, 0,  0, 1, 0, 0,  0, 1, 0, 0],
+        // g  g# a  a#  b  c  c# d   d# e  f  f#  g  g# a  a#  b  c  c# d   d# e  f  f#
+          [0, 1, 0, 0,  1, 0, 0, 0,  0, 1, 0, 0,  0, 1, 0, 0,  1, 0, 0, 0,  0, 1, 0, 0],
+
+        // d  d# e  f   f# g  g# a   a# b  c  c#  d  d# e  f   f# g  g# a   a# b  c  c#
+          [0, 0, 1, 0,  0, 0, 1, 0,  0, 1, 0, 0,  0, 0, 1, 0,  0, 0, 1, 0,  0, 1, 0, 0],
+        // a  a# b  c   c# d  d# e   f  f# g  g#  a  a# b  c   c# d  d# e   f  f# g  g#
+          [0, 0, 1, 0,  0, 0, 0, 1,  0, 0, 0, 1,  0, 0, 1, 0,  0, 0, 0, 1,  0, 0, 0, 1],
+        // e  f  f# g   g# a  a# b   c  c# d  d#  e  f  f# g   g# a  a# b   c  c# d  d#
+          [1, 0, 0, 0,  1, 0, 0, 1,  0, 0, 0, 0,  1, 0, 0, 0,  1, 0, 0, 1,  0, 0, 0, 0]
+        ]);    
+      });
+    });
+  });
+});
