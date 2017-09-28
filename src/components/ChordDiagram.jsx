52c2fe82f57f88d5210cbce7149204e12c662ea1
Derek Mueller <dtmuelle@gmail.com>
2017-09-28 01:32:21 -0700

    render notes on diagram

--- src/components/ChordDiagram.jsx
+++ src/components/ChordDiagram.jsx
@@ -5,26 +5,110 @@ export default class ChordDiagram extends React.Component {
   constructor(props) {
     super(props);
 
-    this.chordDiagrams = new ChordDiagrams({fretCount: 23});
+    this.fretCount = 13;
+    this.chordDiagrams = new ChordDiagrams({
+      fretCount: this.fretCount});
+    this.colorPalette = [
+      'BB453C',
+      '7D9F13',
+      'E7DB84',
+      'A2D2E9',
+      '3EB88D'
+    ];
+    this.chord = ['e', 'g', 'a', 'b', 'd'];
+    this.root = 'e';
 
     this.state = { 
-      diagram: this.chordDiagrams.getDiagram("e g# f#")
+      diagram: this.chordDiagrams.getDiagram(this.chord.join(' '))
     }
   }
 
   fretWidth() {
-    return 1 / (this.chordDiagrams.fretCount) * 100;
+    return 1 / (this.fretCount) * 100;
+  }
+
+  stringSpacing() {
+    return 1 / (this.chordDiagrams.tuning.length) * 100;
   }
 
   renderFrets() {
     return (<div className='frets'>
-      {(new Array(this.chordDiagrams.fretCount).fill(null)).map((_, i) => {
-        return <div className='fret' key={i} style={{width: `${this.fretWidth()}%`}}></div>;
-      })}
+      {(new Array(this.chordDiagrams.fretCount).fill(null)).map(
+        (_, i) => {
+          return <div className='fret' key={i} 
+            style={{width: `${this.fretWidth()}%`}}></div>;
+        })}
     </div>);
   }
 
+  renderNoteLabel(note) {
+    return note.split('').map((elem, i) => {
+      switch(elem) {
+        case '#':
+          return <sup key={i}>{elem}</sup>;
+        default:
+          return <div key={i}>{elem}</div>;
+      }
+    });
+  }
+
+  renderNote(note, i) {
+    return note ? 
+      (<div 
+        className={`note ${this.root === note ? ' root' : ''}`}
+        key={`node-${i}`} 
+        style={{
+          left: `${i * this.fretWidth()}%`,
+          background: 
+            `#${this.colorPalette[this.chord.indexOf(note)]}`
+        }}>{this.renderNoteLabel(note)}
+      </div>) : null;
+  }
+
   renderStrings() {
+    return (
+      this.state.diagram.map((string, i) => {
+        return (
+          <div style={{height: `${this.stringSpacing()}%`}} 
+            className='string-container' key={i}>
+
+            {[<div className='string' key={`string-${i}`}></div>]
+              .concat(
+                string.map((note, j) => {
+                    return this.renderNote(note, j);
+                }))}
+          </div>);
+      }));
+  }
+
+  renderDots() {
+    return [3, 5, 7, 9, 15, 17]
+      .filter((fret) => fret < this.fretCount)
+      .map((fret, i) => {
+        return <div className='dot'
+          key={i}
+          style={{
+            left: 
+              `${fret * this.fretWidth() - this.fretWidth() / 2}%`,
+          }}>
+          </div>;
+      });
+  }
+
+  renderDoubleDots() {
+    return <div className='double-dots'>
+      {[12, 12]
+        .filter((fret) => fret < this.fretCount)
+        .map((fret, i) => {
+          return <div className='dot'
+            key={i}
+            style={{
+              left: 
+                `${fret * this.fretWidth() - this.fretWidth() / 2}%`,
+            }}>
+            </div>;
+        })}
+      </div>;
   }
 
   render() {
@@ -33,6 +117,8 @@ export default class ChordDiagram extends React.Component {
         <div className='fretboard'>
           {this.renderStrings()}
           {this.renderFrets()}
+          {this.renderDots()}
+          {this.renderDoubleDots()}
         </div>
      </div>
     )
