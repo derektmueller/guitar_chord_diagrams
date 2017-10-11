1906545e4c9a0022f4508cd1059b6c6b2466e7e8
Derek Mueller <dtmuelle@gmail.com>
2017-10-11 13:56:07 -0700

    adding feature spec coverage for diagram controls

--- __tests__/features/chordDiagramsFeatureSpec.js
+++ __tests__/features/chordDiagramsFeatureSpec.js
@@ -4,11 +4,59 @@ import { mount } from 'enzyme';
 import App from '../../src/components/App.jsx';
 
 describe("creating chord diagrams", () => {
+  let app;
   beforeEach(() => {
-    let app = mount(<App />);
+    app = mount(<App />);
   });
+
+  function addChordDiagram() {
+    app.find('.controls .title').simulate('change');
+  }
+
+  function deleteChordDiagram() {
+  }
 
   it('the user is able to create chord diagrams', () => {
     expect(true).toEqual(true);
+
+    addChordDiagram({
+      title: 'e minor pentatonic',
+      notes: 'e g a b d',
+      root: 'e',
+      fretCount: 13
+    });
+
+    expect(app.text()).toMatch('e minor pentatonic');
+
+    addChordDiagram({
+      title: 'e minor (guitar)',
+      notes: 'e g b',
+      root: 'e',
+      fretCount: 13
+    });
+
+    expect(app.text()).toMatch('e minor (guitar)');
+
+    addChordDiagram({
+      title: 'e minor (ukulele)',
+      notes: 'e g b',
+      root: 'e',
+      tuning: ['g', 'c', 'e', 'a'],
+      fretCount: 13
+    });
+
+    expect(app.text()).toMatch('e minor (ukulele)');
+
+    deleteChordDiagram('e minor pentatonic');
+
+    expect(app.text()).not.toMatch('e minor pentatonic');
+
+    deleteChordDiagram('e minor');
+
+    expect(app.text()).not.toMatch('e minor (guitar)');
+
+    deleteChordDiagram('e minor');
+
+    expect(app.text()).not.toMatch('e minor (ukulele)');
   });
 });
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -31,39 +31,45 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div id='app'>{[
-        <ChordDiagram 
-          {...new ChordDiagramModel({
-            fretCount: 13, 
-            notes: 'e g a b d',
-            title: "e minor pentatonic",
-            colorPalette: this.colorPalette,
-            root: 'e'
-          })}
-          key='1'
-        />,
-        <ChordDiagram 
-          {...new ChordDiagramModel({
-            fretCount: 13, 
-            notes: 'e g b',
-            title: "e minor",
-            colorPalette: this.colorPalette,
-            root: 'e'
-          })}
-          key='2'
-        />,
-        <ChordDiagram 
-          {...new ChordDiagramModel({
-            fretCount: 13, 
-            notes: 'e g b',
-            title: "e minor",
-            colorPalette: this.colorPalette,
-            tuning: ['g', 'c', 'e', 'a'],
-            root: 'e'
-          })}
-          key='3'
-        />
-      ]}
+      <div id='app'>
+        <div className='controls'>
+          <input className='title' type='text' />
+        </div>
+        <div className='diagrams'>
+        {[
+          <ChordDiagram 
+            {...new ChordDiagramModel({
+              fretCount: 13, 
+              notes: 'e g a b d',
+              title: "e minor pentatonic",
+              colorPalette: this.colorPalette,
+              root: 'e'
+            })}
+            key='1'
+          />,
+          <ChordDiagram 
+            {...new ChordDiagramModel({
+              fretCount: 13, 
+              notes: 'e g b',
+              title: "e minor (guitar)",
+              colorPalette: this.colorPalette,
+              root: 'e'
+            })}
+            key='2'
+          />,
+          <ChordDiagram 
+            {...new ChordDiagramModel({
+              fretCount: 13, 
+              notes: 'e g b',
+              title: "e minor (ukulele)",
+              colorPalette: this.colorPalette,
+              tuning: ['g', 'c', 'e', 'a'],
+              root: 'e'
+            })}
+            key='3'
+          />
+        ]}
+      </div>
     </div>);
   }
 }
